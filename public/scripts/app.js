09e318f976e2c253433f3b903b4c717644aa1683
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-05 13:47:49 -0700

    finally finished sprint one!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,34 +14,34 @@
 
 $(document).ready(function() {
 console.log("Sanity check! app.js running");
+
 $.ajax({
    method: 'GET',
    url: '/api/albums',
-   success: onSuccess,
-   error: onError
+   success: handleReceiveAllAlbums,
+   error: errorReceiveAllAlbums
  });
 
 
-function renderAlbums(albums){
-  var source = $('#album-template').html();
-  var template = Handlebars.compile(source);
-  var newHtml = template(albums);
-  $('#albums').append(newHtml);
-}
 
 
-function onSuccess(album) {
-  //allAlbums = json;
-  album.forEach(function(element){
-    renderAlbums(element);
+function handleReceiveAllAlbums(json) {
+  
+  json.forEach(function (albums){
+    renderAlbums(albums);
   });
 }
-function onError() {
-  console.log('error');
+function errorReceiveAllAlbums() {
+  console.log('Wow, okay.');
 
 }
 
 
 });
 
-// sampleAlbums.forEach(renderAlbum);
+function renderAlbums(albums){
+  var source = $('#album-template').html();
+  var template = Handlebars.compile(source);
+  var newHtml = template(albums);
+  $('#albums').prepend(newHtml);
+}
