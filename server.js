19ba7cba50000b518753254cda1228a9a11ba171
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-04 23:58:54 -0700

    committing just in case i made any changes I cannot remember. Going to bed.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/vendor', express.static(__dirname + '/bower_components'));
 
 var controllers = require('./controllers');
-
+var db = require('./models')
 
 /**********
  * ROUTES *
